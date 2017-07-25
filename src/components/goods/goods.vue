183575f8d14639f277529b7540f11682230af902
vtrust <vtrust32@gmail.com>
2017-07-25 23:56:50 +0800

    first commit

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>I am goods</div>
+</template>
+
+<script type="text/esmascript-6">
+  export default {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header">
+    我是header
+  </div>
+</template>
+
+<script type="text/esmascript-6">
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
